aecb7d075dc6a4059dc01555ab72378c46238c75
Wesley Camargo <camargo.wes@gmail.com>
2022-08-26 19:31:17 +0200

    initial structure

--- src/modules/Microsoft.Resources/resourceGroups/tests/resourceGroup.tests.ps1
+++ src/modules/Microsoft.Resources/resourceGroups/tests/resourceGroup.tests.ps1
@@ -2,9 +2,19 @@ Describe "Resource Group" -Tag resourceGroup {
     Context "Validate Resource Group" {
         It "Should be created successfully" {
             
-            az deployment sub create --template-file .\src\modules\Microsoft.Resources\resourceGroups\main.bicep -l westeurope --parameters .\src\modules\Microsoft.Resources\resourceGroups\examples\resourceGroup.parameters.json
+            # az deployment sub create 
+            # --template-file .\src\modules\Microsoft.Resources\resourceGroups\main.bicep 
+            # -l westeurope 
+            # --parameters .\src\modules\Microsoft.Resources\resourceGroups\examples\resourceGroup.parameters.json
 
-            $resourceGroup = (az group list --query "[?name == 'rg-test']" -o json) | ConvertFrom-Json
+            New-AzSubscriptionDeployment -Location westeurope `
+                -TemplateFile .\src\modules\Microsoft.Resources\resourceGroups\main.bicep `
+                -TemplateParameterFile .\src\modules\Microsoft.Resources\resourceGroups\examples\resourceGroup.parameters.json
+
+            # $resourceGroup = (az group list --query "[?name == 'rg-test']" -o json) | ConvertFrom-Json
+
+            $resourceGroup = Get-AzResourceGroup -Name "rg-test"
+            
             
             $resourceGroup.name | Should -Be "rg-test"
         }
