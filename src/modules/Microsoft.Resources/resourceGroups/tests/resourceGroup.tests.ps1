3cda00b57c3d9e648f161c0d794bf1947b9637b1
Wesley Camargo <camargo.wes@gmail.com>
2022-08-29 17:36:13 +0200

    pipeline version

--- src/modules/Microsoft.Resources/resourceGroups/tests/resourceGroup.tests.ps1
+++ src/modules/Microsoft.Resources/resourceGroups/tests/resourceGroup.tests.ps1
@@ -1,21 +1,10 @@
-BeforeAll {
-    Install-Module -Name Az.Resources -AllowClobber -Scope CurrentUser -Confirm:$false
-}
-
 Describe "Resource Group" -Tag resourceGroup {
     Context "Validate Resource Group" {
         It "Should be created successfully" {
             
-            # az deployment sub create 
-            # --template-file ./src/modules/Microsoft.Resources/resourceGroups/main.bicep 
-            # -l westeurope 
-            # --parameters ./src/modules/Microsoft.Resources/resourceGroups/examples/resourceGroup.parameters.json
-
             New-AzSubscriptionDeployment -Location westeurope `
                 -TemplateFile ./src/modules/Microsoft.Resources/resourceGroups/main.bicep `
                 -TemplateParameterFile ./src/modules/Microsoft.Resources/resourceGroups/examples/resourceGroup.parameters.json
-
-            # $resourceGroup = (az group list --query "[?name == 'rg-test']" -o json) | ConvertFrom-Json
 
             $resourceGroup = Get-AzResourceGroup -Name "rg-test"
             
